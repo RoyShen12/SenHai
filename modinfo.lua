a7440c2a5776f12d5cb0d9757015c0b6392b4e94
死神未満 <royshen12@gmail.com>
2021-12-11 15:50:21 +0800

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -3,7 +3,7 @@ description = [[
 Thanks for PM: 森域
 ]]
 author = "Roy Shen"
-version = "1.57"
+version = "1.58"
 forumthread = ""
 dst_compatible = true --兼容联机
 dont_starve_compatible = false --不兼容单机
