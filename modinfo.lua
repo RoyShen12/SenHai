52a56bac3cb42c11c9664d9283732d8c354c9f1b
死神未満 <royshen12@gmail.com>
2024-01-04 20:02:30 +0800

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -4,10 +4,9 @@ Thanks for PM: 森域
 在战斗栏制造
 可使用各种材料右键对武器使用来强化
 强化材料越稀有，强化经验越高
-自动拾取
-自动采集（20级后升级为自动采摘）
+自动拾取（20级后升级为自动采摘）
 攻击吸血
-几率 AOE
+有几率 AOE
 周期性使用饥饿值来治疗自己
 攻击时有几率对队友释放 HOT 治疗
 减速敌人
@@ -17,7 +16,7 @@ Thanks for PM: 森域
 按End键传送到另一个携带本物品的玩家身边
 ]]
 author = "Roy Shen"
-version = "2.0.3"
+version = "2.0.4"
 forumthread = ""
 dst_compatible = true              --兼容联机
 dont_starve_compatible = false     --不兼容单机
