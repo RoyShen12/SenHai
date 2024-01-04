cde3a869799a3ab50219711ae1e29bb577554ef6
死神未満 <royshen12@gmail.com>
2024-01-04 17:54:46 +0800

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -1,9 +1,23 @@
 name = "猪猪杖"
 description = [[
 Thanks for PM: 森域
+在战斗栏制造
+可使用各种材料右键对武器使用来强化
+强化材料越稀有，强化经验越高
+自动拾取
+自动采集
+攻击吸血
+几率 AOE
+周期性使用饥饿值来治疗自己
+攻击时有几率对队友释放 HOT 治疗
+减速敌人
+可作为斧头、稿子和桨使用
+时刻发光
+按O键开启/关闭锤子和铲子功能
+按End键传送到另一个携带本物品的玩家身边
 ]]
 author = "Roy Shen"
-version = "2.0.2"
+version = "2.0.3"
 forumthread = ""
 dst_compatible = true              --兼容联机
 dont_starve_compatible = false     --不兼容单机
